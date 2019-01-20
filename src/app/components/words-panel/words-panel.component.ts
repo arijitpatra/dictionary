e87da86da7a3f70fb780b856836151a392fdf873
Arijit Patra <ari.clicks@gmail.com>
2019-01-20 16:18:20 +0530

    final code
    final code

--- src/app/components/words-panel/words-panel.component.ts
+++ src/app/components/words-panel/words-panel.component.ts
@@ -12,6 +12,8 @@ export class WordsPanelComponent implements OnInit {
   filterText: string;
   filteredData = [];
   data = [];
+  pageLoader = true;
+  previousSelectedAlphabet: string;
 
   constructor(private dataService: DataServiceService) { }
 
@@ -24,25 +26,50 @@ export class WordsPanelComponent implements OnInit {
 
     this.dataService.getData().subscribe(x => {
       this.data = this.filteredData = x.data;
+      this.pageLoader = false;
     });
   }
 
+  highlightSelectedAlphabet(character?: string) {
+    if (character) {
+      if (this.previousSelectedAlphabet) {
+        document.getElementById(this.previousSelectedAlphabet).style.background = '#fff';
+        document.getElementById(character).style.background = '#f1f1f1';
+      } else {
+        document.getElementById(character).style.background = '#f1f1f1';
+      }
+      this.previousSelectedAlphabet = character;
+    } else {
+      document.getElementById(this.previousSelectedAlphabet).style.background = '#fff';
+    }
+  }
+
   doFilter() {
-      this.filteredData = this.data.filter(x => {
-        if (x.word.includes(this.filterText.toUpperCase())) {
-          return x;
-        }
-      });
-    }
+    this.filteredData = this.data.filter(x => {
+      if (x.word.toUpperCase().includes(this.filterText.toUpperCase())) {
+        return x;
+      }
+    });
+    this.highlightSelectedAlphabet();
+  }
 
-    addToBookmark(word) {
-      this.dataService.addToListBookmark(word);
-    }
+  alphabetBasedFilter(character) {
+    this.filteredData = this.data.filter(x => {
+      if (x.word.toUpperCase().startsWith(character.toUpperCase())) {
+        return x;
+      }
+    });
+    this.highlightSelectedAlphabet(character);
+  }
 
-    playAudio(audioSrc) {
-      const audio = new Audio();
-      audio.src = "../../../assets/bell-ringing-01.mp3";
-      audio.load();
-      audio.play();
-    }
+  addToBookmark(word) {
+    this.dataService.addToListBookmark(word);
+  }
+
+  playAudio(audioSrc) {
+    const audio = new Audio();
+    audio.src = '../../../assets/twi.mp3';
+    audio.load();
+    audio.play();
+  }
 }
